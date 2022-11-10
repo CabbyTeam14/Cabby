6250890a5ce1ca720fead44836e012339b163132
Luka Djordjevic <lukadjordjevicsm01@gmail.com>
2022-11-10 22:56:42 +0100

    Added passenger account activity

---
+++ app/src/main/java/com/example/cabby/activities/PassengerAccountActivity.java
@@ -0,0 +1,16 @@
+package com.example.cabby.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.cabby.R;
+
+public class PassengerAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_passenger_account);
+    }
+}
